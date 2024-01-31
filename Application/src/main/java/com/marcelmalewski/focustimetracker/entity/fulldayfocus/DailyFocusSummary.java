3137502b9c0a3a2aafa4ff3962219d6997431b7f
Marcel Malewski <mmalewski9@gmail.com>
2024-01-31 20:49:44 +0100

    Finished DailyFocusSummary

--- Application/src/main/java/com/marcelmalewski/focustimetracker/entity/fulldayfocus/DailyFocusSummary.java
+++ Application/src/main/java/com/marcelmalewski/focustimetracker/entity/fulldayfocus/DailyFocusSummary.java
@@ -5,9 +5,6 @@ import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-@Getter
-@Setter
 @Entity
 @Table(name = "dailyfocussummary")
 public class DailyFocusSummary {
@@ -24,4 +21,72 @@ public class DailyFocusSummary {
 	//TODO relations
 	private String owner;
 	private String focusSessions;
+
+	@Override
+	public String toString() {
+		return "DailyFocusSummary{" +
+			"id=" + id +
+			'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof DailyFocusSummary that)) return false;
+
+		return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return getId() != null ? getId().hashCode() : 0;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public int getSummedUpFocusTime() {
+		return summedUpFocusTime;
+	}
+
+	public void setSummedUpFocusTime(int summedUpFocusTime) {
+		this.summedUpFocusTime = summedUpFocusTime;
+	}
+
+	public int getNumberOfFinishedFocusSessions() {
+		return numberOfFinishedFocusSessions;
+	}
+
+	public void setNumberOfFinishedFocusSessions(int numberOfFinishedFocusSessions) {
+		this.numberOfFinishedFocusSessions = numberOfFinishedFocusSessions;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getFocusSessions() {
+		return focusSessions;
+	}
+
+	public void setFocusSessions(String focusSessions) {
+		this.focusSessions = focusSessions;
+	}
 }
