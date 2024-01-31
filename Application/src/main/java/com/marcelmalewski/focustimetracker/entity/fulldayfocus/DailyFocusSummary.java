4dd091037aeedb9517a304fb3ffd89a687e0ace6
Marcel Malewski <mmalewski9@gmail.com>
2024-01-31 20:26:31 +0100

    DailyFocusSummary as entity

--- Application/src/main/java/com/marcelmalewski/focustimetracker/entity/fulldayfocus/DailyFocusSummary.java
+++ Application/src/main/java/com/marcelmalewski/focustimetracker/entity/fulldayfocus/DailyFocusSummary.java
@@ -1,4 +1,27 @@
 package com.marcelmalewski.focustimetracker.entity.fulldayfocus;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "dailyfocussummary")
 public class DailyFocusSummary {
+	@Id
+	@SequenceGenerator(name = "dailyfocussummary_sequence", sequenceName = "dailyfocussummary_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dailyfocussummary_sequence")
+	private Long id;
+	@Version
+	private Integer version;
+
+	private int summedUpFocusTime;
+	private int numberOfFinishedFocusSessions;
+
+	//TODO relations
+	private String owner;
+	private String focusSessions;
 }
