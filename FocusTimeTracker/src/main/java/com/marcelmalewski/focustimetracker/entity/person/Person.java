a81b3d390f9ecdd94cbfdbe3aafb89473c110156
Marcel Malewski <mmalewski9@gmail.com>
2024-02-01 22:47:58 +0100

    Updated person with security

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/Person.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/Person.java
@@ -3,15 +3,19 @@ package com.marcelmalewski.focustimetracker.entity.person;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
 @Table(name = "person")
-public class Person {
+public class Person  implements UserDetails {
 	@Id
 	@SequenceGenerator(name = "person_sequence", sequenceName = "person_sequence", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_sequence")
@@ -58,6 +62,36 @@ public class Person {
 	@Override
 	public int hashCode() {
 		return getId() != null ? getId().hashCode() : 0;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.id.toString();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 	public Long getId() {
