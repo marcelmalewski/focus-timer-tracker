3552e837e6a394ccd19deec7ff1008638e62b42f
Marcel Malewski <mmalewski9@gmail.com>
2024-02-03 22:17:38 +0100

    Added tailwind

--- FocusTimeTracker/src/main/frontend/tailwind.config.js
+++ FocusTimeTracker/src/main/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/**/*.html"],
+  content: ["../resources/templates/**/*.html"],
   theme: {
     extend: {
       spacing: {
