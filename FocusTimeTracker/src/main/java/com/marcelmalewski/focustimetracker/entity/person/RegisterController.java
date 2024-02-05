6291e50b16173875506329d431967c4871ea2840
Marcel Malewski <mmalewski9@gmail.com>
2024-02-05 23:10:05 +0100

    Added home and register

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/RegisterController.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/RegisterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "v1")
+@RequestMapping(path = "api/v1")
 @Tag(
 	name = "Registration v1",
 	description = "Person registration v1. Login, and logout are handled by Spring Security"
