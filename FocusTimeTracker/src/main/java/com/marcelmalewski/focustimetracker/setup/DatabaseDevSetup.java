5afde133a13c7c316b8c3a0d36fa39e80c3b5217
Marcel Malewski <mmalewski9@gmail.com>
2024-02-05 22:14:34 +0100

    Added password encodred to setup

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/setup/DatabaseDevSetup.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/setup/DatabaseDevSetup.java
@@ -4,22 +4,25 @@ import com.marcelmalewski.focustimetracker.entity.person.Person;
 import com.marcelmalewski.focustimetracker.entity.person.PersonService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Profile("dev")
 @Component
 public class DatabaseDevSetup implements CommandLineRunner {
 	private final PersonService personService;
+	private final BCryptPasswordEncoder passwordEncoder;
 
-	public DatabaseDevSetup(PersonService personService) {
+	public DatabaseDevSetup(PersonService personService, BCryptPasswordEncoder passwordEncoder) {
 		this.personService = personService;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public void run(String... args) {
 		Person admin = Person.builder()
 			.login("admin")
-			.password("admin.123")
+			.password(passwordEncoder.encode("admin.123"))
 			.email("admin@admin.com")
 			.timerAutoBreak(false)
 			.timerAutoBreak(false)
