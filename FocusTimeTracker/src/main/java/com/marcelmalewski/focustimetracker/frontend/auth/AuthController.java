4d4c051eae308dfd593ff0b85c4bd8a3d20c1b21
Marcel Malewski <mmalewski9@gmail.com>
2024-02-18 14:00:58 +0100

    Added welcome with permissions

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/frontend/auth/AuthController.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/frontend/auth/AuthController.java
@@ -3,12 +3,17 @@ package com.marcelmalewski.focustimetracker.frontend.auth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Tag(name = "Auth", description = "Auth views")
 public class AuthController {
+	@Operation(summary = "Login view")
+	@GetMapping("/welcome")
+	public String getWelcomeView() {
+		return "welcome";
+	}
+
 	@Operation(summary = "Login view")
 	@GetMapping("/login")
 	public String getLoginView() {
@@ -17,7 +22,7 @@ public class AuthController {
 
 	@Operation(summary = "Login view")
 	@GetMapping("/register")
-	public String getRegistrationView(Model model) {
+	public String getRegistrationView() {
 		return "register";
 	}
 }
