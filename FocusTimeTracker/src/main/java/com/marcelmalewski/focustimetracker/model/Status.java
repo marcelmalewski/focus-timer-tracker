1a428b24df4263e09780b74800cf939729c8317b
Marcel Malewski <mmalewski9@gmail.com>
2024-02-18 20:54:01 +0100

    Removed unused

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/model/Status.java
+++
@@ -1,7 +0,0 @@
-package com.marcelmalewski.focustimetracker.model;
-
-public record Status(Integer id,
-										 String userHandle,
-										 String userDisplayName,
-										 String content) {
-}
--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/model/Timer.java
+++
@@ -1,4 +0,0 @@
-package com.marcelmalewski.focustimetracker.model;
-
-public class Timer {
-}
