3cb9985d42c6404344bdb4fe8a6c2bef123d2a8c
Marcel Malewski <mmalewski9@gmail.com>
2024-02-23 22:50:32 +0100

    Added find all

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonController.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonController.java
@@ -1,4 +1,20 @@
 package com.marcelmalewski.focustimetracker.entity.person;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class PersonController {
+	private final PersonService personService;
+
+	public PersonController(PersonService personService) {
+		this.personService = personService;
+	}
+
+	@GetMapping(value = "/api/v1/persons")
+	public List<Person> findAll() {
+		return personService.findAll();
+	}
 }
