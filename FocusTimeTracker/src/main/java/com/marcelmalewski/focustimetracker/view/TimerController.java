a337b42c92fd14bee8440aca19f21d9808cbc031
Marcel Malewski <mmalewski9@gmail.com>
2024-02-27 20:07:05 +0100

    now list of topics is passed to view

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/view/TimerController.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/view/TimerController.java
@@ -2,14 +2,26 @@ package com.marcelmalewski.focustimetracker.view;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TimerController {
-
 	@Operation(summary = "Timer home view")
 	@GetMapping("/home/timer")
-	public String getTimerHomeView() {
+	public String getTimerHomeView(Model model) {
+		List<String> topics = new ArrayList<>();
+		topics.add("Programowanie");
+		topics.add("Rozwój");
+
+		String firstTopic = topics.getFirst();
+		model.addAttribute("firstTopic", firstTopic);
+
+		topics.removeFirst();
+		model.addAttribute("topics", topics);
 		return "timer/timerHome";
 	}
 }
