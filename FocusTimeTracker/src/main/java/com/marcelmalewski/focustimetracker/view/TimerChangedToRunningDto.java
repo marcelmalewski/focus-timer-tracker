2f2b2c4605d6499935cbbbd9edc78ee2663e58f3
Marcel Malewski <mmalewski9@gmail.com>
2024-03-07 20:28:02 +0100

    Removed unused dto

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/view/TimerChangedToRunningDto.java
+++
@@ -1,5 +0,0 @@
-package com.marcelmalewski.focustimetracker.view;
-
-public record TimerChangedToRunningDto(String remainingTime, String remainingTimeAsString,
-																			 String setTimeAsString) {
-}
--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/view/TimerStageChangedWithTimeDto.java
+++
@@ -1,4 +0,0 @@
-package com.marcelmalewski.focustimetracker.view;
-
-public record TimerStageChangedWithTimeDto(Time timerSetTime, Time remainingTime) {
-}
