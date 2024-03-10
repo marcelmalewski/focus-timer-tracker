9d00cd950680df312dde2f797fc013afa6b704e9
Marcel Malewski <mmalewski9@gmail.com>
2024-03-10 11:26:12 +0100

    created findByIdWithFechedMainTopics

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonRepository.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonRepository.java
@@ -1,12 +1,17 @@
 package com.marcelmalewski.focustimetracker.entity.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+	@Query("SELECT person FROM Person person " +
+		"JOIN FETCH person.mainTopics mainTopics Where person.id = ?1 ")
+	Optional<Person> findByIdWithFetchedMainTopics(Long id);
+
 	Optional<Person> findByLoginOrEmail(String login, String email);
 
 	boolean existsByLogin(String login);
