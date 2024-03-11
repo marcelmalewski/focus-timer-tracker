a9059c34dd66b7dd691fd3071a03a1e21afbcb54
Marcel Malewski <mmalewski9@gmail.com>
2024-03-11 22:47:01 +0100

    added updateTimerAutoBreak

--- FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonRepository.java
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/entity/person/PersonRepository.java
@@ -1,8 +1,11 @@
 package com.marcelmalewski.focustimetracker.entity.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -17,4 +20,9 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 	boolean existsByLogin(String login);
 
 	boolean existsByEmail(String email);
+
+	@Modifying(clearAutomatically = true)
+	@Query("UPDATE Person person SET person.timerAutoBreak = :timerAutoBreak WHERE person.id = :id")
+	@Transactional
+	int updateTimerAutoBreak(@Param(value = "id") long id, @Param(value = "timerAutoBreak") boolean timerAutoBreak);
 }
