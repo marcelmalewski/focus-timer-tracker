474317bce555df974b88afc3c7ae60d1d55ee7b3
Marcel Malewski <mmalewski9@gmail.com>
2024-05-12 21:02:26 +0200

    Added converter and stage

---
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/converter/StageConverter.java
@@ -0,0 +1,31 @@
+package com.marcelmalewski.focustimetracker.converter;
+
+import com.marcelmalewski.focustimetracker.view.Stage;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class StageConverter implements AttributeConverter<Stage, String> {
+
+	@Override
+	public String convertToDatabaseColumn(Stage maybeStage) {
+		return switch (maybeStage) {
+			case null -> null;
+			case Stage stage -> stage.getStageName();
+		};
+	}
+
+	// TODO stworzyć lepszy exception
+	@Override
+	public Stage convertToEntityAttribute(String maybeStageName) {
+		return switch (maybeStageName) {
+			case null -> null;
+			case String stageName -> Stream.of(Stage.values())
+				.filter(s -> s.getStageName().equals(stageName))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
+		};
+	}
+}
---
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/view/Stage.java
@@ -0,0 +1,19 @@
+package com.marcelmalewski.focustimetracker.view;
+
+public enum Stage {
+	HOME("Home"),
+	FOCUS("Focus"),
+	PAUSE("Pause"),
+	SHORT_BREAK("Short break"),
+	LONG_BREAK("Long break");
+
+	private final String stageName;
+
+	private Stage(String stageName) {
+		this.stageName = stageName;
+	}
+
+	public String getStageName() {
+		return stageName;
+	}
+}
