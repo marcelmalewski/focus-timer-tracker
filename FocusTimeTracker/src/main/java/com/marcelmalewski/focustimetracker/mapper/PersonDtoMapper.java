6b011c7a3d819e18229860e8aa63a9d41a8a80d1
Marcel Malewski <mmalewski9@gmail.com>
2024-06-02 20:20:08 +0200

    Created mapper

---
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/mapper/PersonDtoMapper.java
@@ -0,0 +1,38 @@
+package com.marcelmalewski.focustimetracker.mapper;
+
+import com.marcelmalewski.focustimetracker.entity.person.dto.PrincipalBasicDataWithMainTopicsDto;
+import com.marcelmalewski.focustimetracker.view.dto.TimerPauseDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public abstract class PersonDtoMapper {
+	public TimerPauseDto toTimerPauseDto(PrincipalBasicDataWithMainTopicsDto principalBasicDataWithMainTopicsDto) {
+		if (principalBasicDataWithMainTopicsDto == null) {
+			return null;
+		}
+
+		String setTimePretty = principalBasicDataWithMainTopicsDto.timerSetHours() + "h " +
+			principalBasicDataWithMainTopicsDto.timerSetMinutes() + "m " +
+			principalBasicDataWithMainTopicsDto.timerSetSeconds() + "s";
+
+		int remainingTime = principalBasicDataWithMainTopicsDto.timerRemainingTime();
+		int remainingTimeSeconds = remainingTime % 60;
+		int remainingTimeMinutes = (remainingTime / 60) % 60;
+		int remainingTimeHours = remainingTime / 60 / 60;
+
+		String remainingTimePretty = remainingTimeHours + "h " +
+			remainingTimeMinutes + "m " +
+			remainingTimeSeconds + "s";
+
+
+		return new TimerPauseDto(
+			setTimePretty,
+			remainingTimePretty,
+			principalBasicDataWithMainTopicsDto.timerRemainingTime(),
+			principalBasicDataWithMainTopicsDto.timerSelectedTopic(),
+			principalBasicDataWithMainTopicsDto.timerShortBreak().toString(),
+			principalBasicDataWithMainTopicsDto.timerLongBreak().toString(),
+			principalBasicDataWithMainTopicsDto.timerAutoBreak()
+		);
+	}
+}
---
+++ FocusTimeTracker/src/main/java/com/marcelmalewski/focustimetracker/mapper/PersonMapper.java
@@ -0,0 +1,12 @@
+package com.marcelmalewski.focustimetracker.mapper;
+
+import com.marcelmalewski.focustimetracker.entity.person.Person;
+import com.marcelmalewski.focustimetracker.entity.person.dto.PrincipalBasicDataDto;
+import com.marcelmalewski.focustimetracker.entity.person.dto.PrincipalBasicDataWithMainTopicsDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+	PrincipalBasicDataDto toPrincipalBasicDataDto(Person person);
+	PrincipalBasicDataWithMainTopicsDto toPrincipalBasicDataWithMainTopicsDto(Person person);
+}
